ba52870dfa05e87c23f6609219cb72f884e7533c
Humza Haider <haide043@morris.umn.edu>
2016-04-05 21:39:12 -0500

    Map now appears with marker (cannot have multiple markers.

--- server.R
+++ server.R
@@ -1,1 +1,13 @@
-#server
+# server.R  
+library(shiny)
+
+shinyServer(function(input, output, session) {  
+  
+  library(shiny)
+  shinyServer(function(input, output) {
+    output$distPlot <- renderPlot({
+      dist <- rnorm(input$obs)
+      hist(dist)
+    })
+  })
+  })
--- ui.R
+++ ui.R
@@ -1,1 +1,16 @@
-#ui
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("New Application"),
+  sidebarPanel(
+    sliderInput("obs", 
+                "Number of observations:", 
+                min = 1, 
+                max = 1000, 
+                value = 500)
+  ),
+  mainPanel(
+    plotOutput("distPlot"),
+    tags$head(HTML("<script type='text/javascript' src='GoogleMap.js'></script>")),
+    includeHTML("index.html"))
+))
+
